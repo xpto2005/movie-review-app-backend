f96b893f36f60aacaba08fc2c82d6bb807f63865
xpto2005 <pauloxpto19@gmail.com>
2025-11-17 21:06:00 +0100

    Update Home page UI and routing improvements

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,25 +1,13 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  movie: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Movie',
-    required: true,
+const reviewSchema = new mongoose.Schema(
+  {
+    movie: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+    author: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true }
   },
-  reviewer: {
-    type: String,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Review', reviewSchema);
