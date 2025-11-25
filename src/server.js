223fa0a8bbc5f732d0d8224c66d3530461692e50
xpto2005 <pauloxpto19@gmail.com>
2025-11-25 20:46:52 +0100

    .env.example && new  server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = 5000;
+// LÊ A PORTA DO .env OU USA 5000 POR DEFICE
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
